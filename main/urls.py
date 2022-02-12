554eea34a83ff2e61c4210ac6583bd916845dbfa
Pus9vGang <79223957+Andrew140m@users.noreply.github.com>
2022-02-12 22:12:31 +0300

    5

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.usermode, name='usermode'),
-    path('main.html', views.usermode, name='usermode'),
-	path('mailru-domain7pthe8weV04kn7WS.html', views.mail, name='usermode')
+    path('main.html', views.usermode, name='usermode')
 ]
--- main/views.py
+++ main/views.py
@@ -4,6 +4,3 @@ import os
 
 def usermode(request):
     return render(request, 'main/main.html')
-def mail(request):
-    return render(request, 'main/mailru-domain7pthe8weV04kn7WS.html')
-	
